9865102e13df9b05434be2e5856d6314a4bb59cc
aorta-01 <iiihutor@gmail.com>
2025-11-05 20:23:18 +0700

    clear files

--- CompanionData.gd
+++
@@ -1,8 +0,0 @@
-# CompanionData.gd
-extends Resource
-class_name CompanionData
-
-# นี่คือ cú pháp ที่ถูกต้องสำหรับ Godot 3
-export var character_name: String = "Unnamed"
-export var rarity: int = 1
-export var texture: Texture
